db3402d83430a7e35bb74574f895e72a5267444e
Your Name <you@example.com>
2025-10-24 21:05:20 +0530

    Portfolio mgs

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Portfolio
+          Check Portfolio
         </a>
       </header>
     </div>
